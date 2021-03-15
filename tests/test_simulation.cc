c428a37afa281759ff48871dc1242ced59557980
jonah <jonahlt2@illinois.edu>
2021-03-15 22:29:55 +0700

    created collision test section.

--- tests/test_simulation.cc
+++ tests/test_simulation.cc
@@ -1,16 +1,36 @@
 #include <catch2/catch.hpp>
-
+#include <particle.h>
 #include <gas_container.h>
 
 using idealgas::GasContainer;
+using glm::vec2;
 
-TEST_CASE("Colliding with each other") {
-  GasContainer container = GasContainer(vec2(20,20),
-                                        vec2(21.4, 21.4),
-                                        vec2(0.1, 0),
-                                        vec2(-0.1, 0), 40);
-  container.AdvanceOneFrame();
-  REQUIRE(container.GetParticles().at(1).GetVelocity().y == (float)0.1);
+//https://www.tutorialspoint.com/floating-point-comparison-in-cplusplus
+bool compare_float(float x, float y, float epsilon = 0.0001f){
+  if(fabs(x - y) < epsilon)
+    return true; //they are same
+  return false; //they are not same
+}
+
+TEST_CASE("Colliding") {
+  SECTION("Colliding with each other particle 2") {
+    GasContainer container = GasContainer(vec2(20,20),
+                                          vec2(21.4, 21.4),
+                                          vec2(0.1, 0),
+                                          vec2(-0.1, 0), 40);
+    container.AdvanceOneFrame();
+    REQUIRE(compare_float(glm::length(container.GetParticles().at(1).GetVelocity()), 0.1f));
+  }
+
+  SECTION("Colliding with a wall") {
+    GasContainer container = GasContainer(vec2(39.9,20),
+                                          vec2(21.4, 21.4),
+                                          vec2(0.1, 0),
+                                          vec2(-0.1, 0), 40);
+    container.AdvanceOneFrame();
+    REQUIRE(compare_float(glm::length(container.GetParticles().at(1).GetVelocity()), -0.1f));
+  }
+
 }
 
 
